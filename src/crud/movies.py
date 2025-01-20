9c25e100b6d970387c060522a08f89450f7fcde6
Tsvetan Kichuk <toys4babyodua@gmail.com>
2025-01-20 19:29:15 +0100

    Add CRUD operations for movies

---
+++ src/crud/movies.py
@@ -0,0 +1,59 @@
+from database import get_db
+from fastapi import Depends, Query
+from models import MovieModel
+from schemas import MovieCreateSchema, MovieUpdateSchema
+from sqlalchemy.orm import Session
+
+
+def create_movie(db: Session, movie: MovieCreateSchema):
+    db_movie = MovieModel(**movie.dict())
+    db.add(db_movie)
+    db.commit()
+    db.refresh(db_movie)
+    return db_movie
+
+
+def get_movie(movie_id: int, db: Session = Depends(get_db)):
+    movie = db.query(MovieModel).filter(MovieModel.id == movie_id).first()
+    return movie
+
+
+def get_movies(
+        page: int = Query(default=1, ge=1, description="The page number(>= 1)"),
+        per_page: int = Query(default=10, ge=1, le=20, description="Film quantity at the page (>= 1 и <= 20)"),
+        db: Session = Depends(get_db)
+):
+    total_items = db.query(MovieModel).count()
+    offset = (page - 1) * per_page
+    movies = db.query(MovieModel).order_by(MovieModel.id.desc()).offset(offset).limit(per_page).all()
+    base_url = "/movies/"
+    prev_page = f"{base_url}?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"{base_url}?page={page + 1}&per_page={per_page}" if offset + per_page < total_items else None
+    return {
+        "movies": movies,
+        "prev_page": prev_page,
+        "next_page": next_page,
+        "total_pages": (total_items + per_page - 1) // per_page,
+        "total_items": total_items,
+    }
+
+
+def update_movie(db: Session, movie_id: int, movie: MovieUpdateSchema):
+    db_movie = db.query(MovieModel).filter(MovieModel.id == movie_id).first()
+    if not db_movie:
+        return None
+    db_movie.title = movie.title
+    db_movie.genre = movie.genre
+    db_movie.price = movie.price
+    db.commit()
+    db.refresh(db_movie)
+    return db_movie
+
+
+def delete_movie(db: Session, movie_id: int):
+    db_movie = db.query(MovieModel).filter(MovieModel.id == movie_id).first()
+    if not db_movie:
+        return None
+    db.delete(db_movie)
+    db.commit()
+    return db_movie
