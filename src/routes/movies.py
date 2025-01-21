953e0980bb1ea3c7566490b9c93d7457ec1db8f0
Tsvetan Kichuk <toys4babyodua@gmail.com>
2025-01-21 20:54:50 +0100

    Refactor and enhance movie routes for clarity and validation.

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -3,6 +3,7 @@ from database import get_db
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from src.database.models import MovieModel
 from src.schemas.movies import MovieReadSchema, MovieCreateSchema, \
     MovieUpdateSchema
 
@@ -10,9 +11,20 @@ router = APIRouter()
 
 
 @router.post("/movies/", response_model=MovieReadSchema)
-def add_film(film: MovieCreateSchema, db: Session = Depends(get_db)):
-    return create_movie(db, film)
-
+def add_movie(movie: MovieCreateSchema, db: Session = Depends(get_db)):
+    existing_movie = (
+        db.query(MovieModel)
+        .filter(MovieModel.name == movie.name, MovieModel.date == movie.date)
+        .first()
+    )
+    if existing_movie:
+        raise HTTPException(
+            status_code=409,
+            detail=f"A movie with the name '{movie.name}' and release date '{movie.date}' already exists."
+        )
+    if existing_movie is None:
+        raise HTTPException(status_code=400)
+    return create_movie(db, movie)
 
 @router.get("/movies/", response_model=list[MovieReadSchema])
 def list_movies(db: Session = Depends(get_db)):
@@ -28,21 +40,21 @@ def list_movies(db: Session = Depends(get_db)):
 def read_movie(movie_id: int, db: Session = Depends(get_db)):
     db_movie = get_movie(db, movie_id)
     if not db_movie:
-        raise HTTPException(status_code=404, detail="Movie not found")
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
     return db_movie
 
 
 @router.put("/movies/{movie_id}", response_model=MovieReadSchema)
-def edit_film(film_id: int, film: MovieUpdateSchema, db: Session = Depends(get_db)):
-    db_movie = update_movie(db, film_id, film)
+def edit_movie(movie_id: int, movie: MovieUpdateSchema, db: Session = Depends(get_db)):
+    db_movie = update_movie(db, movie_id, movie)
     if not db_movie:
         raise HTTPException(status_code=404, detail="Movie not found")
     return db_movie
 
 
 @router.delete("/movies/{movie_id}", response_model=MovieReadSchema)
-def remove_film(movie_id: int, db: Session = Depends(get_db)):
+def remove_movie(movie_id: int, db: Session = Depends(get_db)):
     db_movie = delete_movie(db, movie_id)
     if not db_movie:
-        raise HTTPException(status_code=404, detail="Film not found")
+        raise HTTPException(status_code=404, detail="Movie not found")
     return db_movie
