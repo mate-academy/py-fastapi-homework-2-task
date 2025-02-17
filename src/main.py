915d289cc524cadfb99f9c77cf282af189c84b03
Arsen Arutiunov <arsen.test.mess@gmail.com>
2025-02-17 20:50:58 +0200

    Solution

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-
-from routes import movie_router
+from routes.movies import router as movie_router
 
 app = FastAPI(
     title="Movies homework",
@@ -9,4 +8,6 @@ app = FastAPI(
 
 api_version_prefix = "/api/v1"
 
-app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
+app.include_router(movie_router,
+                   prefix=f"{api_version_prefix}/theater",
+                   tags=["theater"])
