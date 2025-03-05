8131af86265f20af062c763d6db72c848bad2497
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-03-05 21:23:15 +0200

    Solution

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,119 @@
-# Write your code here
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import date
+
+
+class GenreBase(BaseModel):
+    id: int
+    name: str
+    
+    class Config:
+        orm_mode = True
+        from_attriubtes = True
+
+
+class ActorBase(BaseModel):
+    id: int
+    name: str
+    
+    class Config:
+        orm_mode = True
+        from_attriubtes = True
+
+
+class CountryBase(BaseModel):
+    id: int
+    code: str
+    name: str
+    
+    class Config:
+        orm_mode = True
+        from_attriubtes = True
+
+
+class LenguageBase(BaseModel):
+    id: int
+    name: str
+    
+    class Config:
+        orm_mode = True
+        from_attriubtes = True
+
+
+class MoviesListItemSchema(BaseModel):
+    id: int
+    name: str
+    date: date
+    score: float
+    overview: str
+    
+    class Config:
+        orm_mode = True
+        from_attriubtes = True
+
+
+class MovieDetailSchema(BaseModel):
+    id: int
+    name: str
+    date: date
+    score: float
+    overview: str
+    status: str
+    budget: float
+    revenue: float
+    country: CountryBase
+    lenguages: list[LenguageBase]
+    actors: list[ActorBase]
+    genres: list[GenreBase]
+    
+    class Confid:
+        orm_mode = True
+        from_attriubtes = True
+
+
+class MoviesListResponseSchema(BaseModel):
+    movies: List[MoviesListItemSchema]
+    prev_page: Optional[int]
+    next_page: Optional[int]
+    total_pages: int
+    total_items: int
+
+
+class MovieCreateRequestShema(BaseModel):
+    name: str = Field(..., max_length=155)
+    date: date 
+    score: float = Field(..., ge=0, le=100)
+    overview: str
+    status: str = Field(
+        ...,
+        pattern=
+        "^(Rumored|Planned|In Production|Post Production|Released|Canceled)$"
+        )
+    budget: float = Field(..., ge=0)
+    revenue: float = Field(..., ge=0)
+    country_id: str = Field(..., min_length=2, max_length=2)
+    lenguages: list[str]
+    actors: list[str]
+    genres: list[str]
+    
+
+class MovieUpdateRequestSchema(BaseModel):
+    name: Optional[str] = Field(None, max_length=155)
+    date: Optional[date]
+    score: Optional[float] = Field(None, ge=0, le=100)
+    overview: Optional[str]
+    status: Optional[str] = Field(
+        None,
+        pattern=
+        "^(Rumored|Planned|In Production|Post Production|Released|Canceled)$"
+        )
+    budget: Optional[float] = Field(None, ge=0)
+    revenue: Optional[float] = Field(None, ge=0)
+    country_id: Optional[str] = Field(None, min_length=2, max_length=2)
+    lenguages: Optional[list[str]]
+    actors: Optional[list[str]]
+    genres: Optional[list[str]]
+
+
+class MovieDeleteRequestSchema(BaseModel):
+    id: int
