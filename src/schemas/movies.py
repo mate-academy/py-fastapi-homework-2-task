0731a33add599a48062e2c4687e3a39f9ccdf70b
taraskhom <khomenkotaras1305@gmail.com>
2025-03-13 14:33:39 +0200

    add schemas for every entity

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,103 @@
-# Write your code here
+from datetime import date
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class GenreSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+class ActorSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+class LanguageSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+class CountrySchema(BaseModel):
+    id: int
+    code: str
+    name: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+
+class MovieListItemSchema(BaseModel):
+    id: int
+    name: str
+    date: date
+    score: float
+    overview: str
+
+    class Config:
+        orm_mode = True
+        model_config = {"from_attributes": True}
+
+
+class MovieDetailSchema(BaseModel):
+    id: int
+    name: str
+    date: date
+    score: float
+    overview: str
+    status: str
+    budget: float
+    revenue: float
+    country: CountrySchema
+    genres: List[GenreSchema]
+    actors: List[ActorSchema]
+    languages: List[LanguageSchema]
+
+    class Config:
+        orm_mode = True
+        model_config = {"from_attributes": True}
+
+
+class MovieCreateSchema(BaseModel):
+    name: str
+    date: date
+    score: float
+    overview: str
+    status: str
+    budget: float
+    revenue: float
+    country: str
+    genres: List[str]
+    actors: List[str]
+    languages: List[str]
+
+
+class MovieUpdateSchema(BaseModel):
+    name: Optional[str] = None
+    date: Optional[date] = None
+    score: Optional[float] = None
+    overview: Optional[str] = None
+    status: Optional[str] = None
+    budget: Optional[float] = None
+    revenue: Optional[float] = None
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieListItemSchema]
+    total_pages: int
+    total_items: int
+    prev_page: Optional[str]
+    next_page: Optional[str]
+
+    class Config:
+        orm_mode = True
+        model_config = {"from_attributes": True}
