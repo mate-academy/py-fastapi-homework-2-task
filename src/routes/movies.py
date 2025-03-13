a3af5c58ca199b17e1a9cae057c26dd50c89440b
Julia Alexeeva <julia4406@gmail.com>
2025-03-14 00:52:12 +0200

    Add: add_movie endpoint. don't work

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -2,12 +2,49 @@ from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select, func
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, selectinload
+from starlette import status
 
+from crud import get_all_movies_paginated, get_movie, create_movie
 from database import get_db, MovieModel
-from database.models import CountryModel, GenreModel, ActorModel, LanguageModel
-
+from database.models import LanguageModel, ActorModel, GenreModel, CountryModel
+from schemas import MovieDetailSchema, MovieListResponseSchema
+from schemas.movies import MovieCreateSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/{movie_id}/")
+async def retrieve_movie(
+        movie_id: int,
+        db: AsyncSession = Depends(get_db)
+):
+    movie = await get_movie(movie_id, db)
+
+    if not movie:
+        raise HTTPException(
+            status_code=404, detail="Movie with the given ID was not found."
+        )
+    return movie
+
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_list_of_movies(
+        db: AsyncSession = Depends(get_db),
+        page: int = Query(ge=1, default=1),
+        per_page: int = Query(ge=1, le=20, default=10)
+):
+    response = await get_all_movies_paginated(db, page, per_page)
+
+    if not response["movies"]:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    return response
+
+
+@router.post("/movies/", response_model=MovieDetailSchema)
+async def add_movie(
+        movie: MovieCreateSchema,
+        db: AsyncSession = Depends(get_db)
+):
+    return await create_movie(movie, db)
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,103 @@
-# Write your code here
+from pydantic import BaseModel, constr, confloat
+from datetime import date
+
+from database.models import MovieStatusEnum
+
+
+class GenreModelSchema(BaseModel):
+    id: int
+    name: str
+    movies: list["MovieDetailSchema"]
+
+    class Config:
+        from_attributes = True
+
+
+class ActorModelSchema(BaseModel):
+    id: int
+    name: str
+    movies: list["MovieDetailSchema"]
+
+    class Config:
+        from_attributes = True
+
+
+class CountryModelSchema(BaseModel):
+    id: int
+    code: constr(max_length=3)
+    name: str
+    movies: list["MovieDetailSchema"]
+
+    class Config:
+        from_attributes = True
+
+
+class LanguageModelSchema(BaseModel):
+    id: int
+    name: str
+    movies: list["MovieDetailSchema"]
+
+    class Config:
+        from_attributes = True
+
+
+class MovieBaseSchema(BaseModel):
+    name: str
+    date: date
+    score: confloat(ge=0, le=100)
+    overview: str
+    status: MovieStatusEnum
+    budget: float
+    revenue: float
+    country_id: int
+
+    country: "CountryModelSchema"
+    genres: list["GenreModelSchema"]
+    actors: list["ActorModelSchema"]
+    languages: list["LanguageModelSchema"]
+
+    class Config:
+        from_attributes = True
+
+
+class MovieListItemSchema(BaseModel):
+    id: int
+    name: str
+    date: date
+    score: confloat(ge=0, le=100)
+    overview: str
+
+    class Config:
+        from_attributes = True
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: list[MovieListItemSchema]
+    prev_page: str | None = None
+    next_page: str | None = None
+    total_items: int
+    total_pages: int
+
+    class Config:
+        from_attributes = True
+
+
+class MovieDetailSchema(MovieBaseSchema):
+    id: int
+
+
+class MovieCreateSchema(BaseModel):
+    name: str
+    date: date
+    score: confloat(ge=0, le=100)
+    overview: str
+    status: MovieStatusEnum
+    budget: float
+    revenue: float
+    country: str
+    genres: list[str]
+    actors: list[str]
+    languages: list[str]
+
+    class Config:
+        from_attributes = True
