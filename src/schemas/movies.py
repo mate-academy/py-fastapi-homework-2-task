f7ea503d106e857309fab51982b0f73c93fba106
Anton Konyk <antonkonyk.com>
2025-04-04 21:37:18 +0200

    create Genre models

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,19 @@
 # Write your code here
+from typing import List, Optional
+from pydantic import BaseModel, Field
+class GenreModel(BaseModel):
+    name: str
+    movies: list["MovieDetailSchema"]
+
+
+class GenreDetail(GenreModel):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class GenreDetailResponse(BaseModel):
+    id: int
+    name: str
+
