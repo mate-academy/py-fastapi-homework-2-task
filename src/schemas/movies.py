c3b2e5c7949564bc29725bb4439a113076cfbf87
Anton Konyk <antonkonyk.com>
2025-04-04 21:40:02 +0200

    create Country models

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,7 +1,11 @@
-# Write your code here
+from datetime import date
 from typing import List, Optional
 
 from pydantic import BaseModel, Field
+
+from database.models import MovieStatusEnum
+
+
 class GenreModel(BaseModel):
     name: str
     movies: list["MovieDetailSchema"]
@@ -34,4 +38,23 @@ class ActorDetail(ActorModel):
 class ActorDetailResponse(BaseModel):
     id: int
     name: str
+
+
+class CountryModel(BaseModel):
+    code: str
+    name: Optional[str] = None
+    movies: list["MovieDetailSchema"]
+
+
+class CountryDetail(CountryModel):
+    int: int
+
+    class Config:
+        from_attributes = True
+
+
+class CountryDetailResponse(BaseModel):
+    id: int
+    code: str
+    name: Optional[str] = None
 
