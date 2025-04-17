ae59a68c521f7737314ef4931bbff9b3fb7c707f
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-04-17 08:09:50 +0300

    feat: create all schemas

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,94 @@
-# Write your code here
+from datetime import date
+from typing import Optional, List
+
+from pydantic import BaseModel, Field
+
+from database.models import MovieStatusEnum
+
+
+class MovieBase(BaseModel):
+    name: str = Field(None, max_length=255)
+    date: date
+    score: float = Field(ge=0, le=100)
+    overview: str
+
+
+class MovieListItemSchema(MovieBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieListItemSchema]
+    prev_page: Optional[str] = None
+    next_page: Optional[str] = None
+    total_pages: Optional[int] = None
+    total_items: Optional[int] = None
+
+
+class MovieUpdateSchema(BaseModel):
+    name: Optional[str] = Field(None, max_length=255)
+    date: Optional[date] = None
+    score: Optional[float] = Field(None, ge=0, le=100)
+    overview: Optional[str] = None
+    status: Optional[MovieStatusEnum] = None
+    budget: Optional[float] = Field(None, ge=0)
+    revenue: Optional[float] = Field(None, ge=0)
+
+
+class MovieCreateSchema(MovieBase):
+    status: MovieStatusEnum
+    budget: float = Field(None, ge=0)
+    revenue: float = Field(None, ge=0)
+    country: str
+    genres: List[str]
+    actors: List[str]
+    languages: List[str]
+
+
+class GenreReadSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class ActorReadSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class CountryReadSchema(BaseModel):
+    id: int
+    code: str
+    name: Optional[str] = None
+
+    class Config:
+        from_attributes = True
+
+
+class LanguageReadSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class MovieDetailSchema(MovieListItemSchema):
+    status: MovieStatusEnum
+    budget: float
+    revenue: float
+    country: CountryReadSchema
+    genres: List[GenreReadSchema]
+    actors: List[ActorReadSchema]
+    languages: List[LanguageReadSchema]
+
+    class Config:
+        from_attributes = True
