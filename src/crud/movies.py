20448a4fde589fa5a6aa8edd32f78da6f78e0560
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-04-17 10:15:27 +0300

    feat: create get_country and create_country crud operations

--- src/crud/movies.py
+++ src/crud/movies.py
@@ -4,7 +4,8 @@ from sqlalchemy import select, func, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import MovieModel
-from schemas.movies import MovieCreateSchema
+from database.models import CountryModel
+from schemas.movies import CountryCreateSchema
 
 
 async def get_movies(db: AsyncSession, page: int = 1, per_page: int = 10):
@@ -38,3 +39,18 @@ async def check_if_movie_exist(
     result = await db.execute(query)
     movie = result.scalars().first()
     return movie
+
+
+async def get_country(db: AsyncSession, county_code: str):
+    query = select(CountryModel).where(CountryModel.code == county_code)
+    result = await db.execute(query)
+    country = result.scalar_one_or_none()
+    return country
+
+
+async def create_country(db: AsyncSession, country: CountryCreateSchema):
+    new_country = CountryModel(**country.model_dump())
+    db.add(new_country)
+    await db.commit()
+    await db.refresh(new_country)
+    return new_country
