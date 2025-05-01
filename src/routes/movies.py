4cd41ae8c58d5f9fcefbf218d78e1c3138610da7
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-05-01 21:39:06 +0300

    add movies list endpoint

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,3 +1,6 @@
+from math import ceil
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select, func
 from sqlalchemy.exc import IntegrityError
@@ -6,8 +9,53 @@ from sqlalchemy.orm import joinedload
 
 from database import get_db, MovieModel
 from database.models import CountryModel, GenreModel, ActorModel, LanguageModel
-
+from schemas import MovieListResponseSchema
+from schemas.movies import MovieListItemSchema
 
 router = APIRouter()
 
-# Write your code here
+NO_MOVIES_ERROR = "No movies found."
+
+@router.get(
+    "/movies/",
+    response_model=MovieListResponseSchema,
+    responses={
+        404: {
+            "content": {
+                "application/json": {"example": {"detail": NO_MOVIES_ERROR}}
+            },
+        }
+    },
+)
+async def get_movies(
+        db: AsyncSession = Depends(get_db),
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, ge=1),
+):
+    total = await db.scalar(select(func.count()).select_from(MovieModel))
+    total_pages = ceil(total / per_page)
+
+    if total == 0 or page > total_pages:
+        raise HTTPException(status_code=404, detail=NO_MOVIES_ERROR)
+
+    result = await db.execute(
+        select(MovieModel)
+        .order_by(MovieModel.id)
+        .limit(per_page)
+        .offset((page - 1) * per_page)
+    )
+    movies = result.scalars().all()
+
+    url_ = "/theater/movies?page="
+    next_page = (
+        f"{url_}{page + 1}&per_page={per_page}" if page < total_pages else None
+    )
+    prev_page = f"{url_}{page - 1}&per_page={per_page}" if page > 1 else None
+
+    return MovieListResponseSchema(
+        movies=movies,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total,
+    )
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,36 @@
-# Write your code here
+import datetime
+from decimal import Decimal
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from database.models import MovieStatusEnum
+
+
+class MovieListItemSchema(BaseModel):
+    id: int
+    name: str
+    date: datetime.date
+    score: float
+    overview: str
+    # status: MovieStatusEnum
+    # budget: Decimal
+    # revenue: float
+    # country_id: int
+    # genres_ids: List[int] = []
+    # actors_ids: List[int] = []
+    # languages_ids: List[int] = []
+
+    class Config:
+        from_attributes = True
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieListItemSchema] = []
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
+
+    class Config:
+        from_attributes = True
