29f6c3ecb9abd22fdd4ca3f8bc6f55aa4359d755
Yevhenii0536 <eugeniy0536@gmail.com>
2025-05-06 12:40:27 +0300

    Solution

--- src/main.py
+++ src/main.py
@@ -11,3 +11,7 @@ app = FastAPI(
 api_version_prefix = "/api/v1"
 
 app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
+
+
+if __name__ == "__main__":
+    import uvicorn
